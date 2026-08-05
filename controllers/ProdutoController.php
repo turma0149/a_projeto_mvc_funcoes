16c356bd62dc4b9e90a7c7ad0f03badaf149d206
Michelle Belli <progweb0149@gmail.com>
2026-08-05 19:07:27 -0300

    Validador php

--- controllers/ProdutoController.php
+++ controllers/ProdutoController.php
@@ -3,6 +3,9 @@
 //A resposta será enviada em formato JSON
 header("Content-Type: application/json; charset=utf-8");
 
+// Carrega a classe Validator.
+require_once __DIR__ . "/../libs/Validator.php";
+
 //Verifica se a requisição é do tipo POST
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     http_response_code(405); //405 - método não permitido
@@ -15,36 +18,51 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     exit;
 }
 
-// Recebe os dados enviados pelo formulário
-$nome = trim($_POST['nome']);
-$categoria = trim($_POST['categoria']);
-$preco = trim($_POST['preco']);
-$quantidade = trim($_POST['quantidade']);
+// Cria o objeto validador
+$validator = new Validator($_POST);
 
-// Valida os campos obrigatórios 
-if ($nome === "" || $categoria === "" || $preco === "" || $quantidade === "") {
-    http_response_code(400);
+//Executa a função que contém as regras de validação
+validarCadastro($validator);
+
+
+// -------->>> TODO: Aqui seria o banco de dados 
+
+
+//Verifica se tem erros
+if ($validator->fails()) {
+
+    http_response_code(422);
 
     echo json_encode([
         "sucesso" => false,
-        "mensagem" => "Preencha todos os campos"
-    ]);
+        "mensagem" => "Corrija os campos indicados.",
+        "erros" => $validator->errors()
+    ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 
     exit;
 }
 
-// -------->>> TODO: Aqui seria o banco de dados 
 
 //Retornar sucesso 
 http_response_code(200);
 
 echo json_encode([
     "sucesso" => true,
-    "mensagem" => "Produto cadastrado com sucesso!",
-    "produto" => [
-        "nome" => $nome,
-        "categoria" => $categoria,
-        "preco" => $preco,
-        "quantidade" => $quantidade,
-    ]
-]);
+    "mensagem" => "Dados validados com sucesso.",
+    "dados" => $validator->data()
+], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+exit;
+
+
+
+// ------ Funções auxiliares ------------
+
+
+function validarCadastro($validator)
+{
+    $validator->required("nome", "Informe o nome.");
+    $validator->string("nome", "O nome deve ser um texto.");
+    $validator->minLength("nome", 3, "O nome deve ter pelo menos 3 caracteres.");
+    $validator->maxLength("nome", 30, "O nome deve ter no máximo 30 caracteres.");
+}
