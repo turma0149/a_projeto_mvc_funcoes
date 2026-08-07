d97aa67c1e9e86a4313ea8d4edac53040f99b4b8
Michelle Belli <progweb0149@gmail.com>
2026-08-07 09:27:47 -0300

    ajustes

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
     </main>
 
     <!-- Rodapé -->
-    <footer class="bg-dark text-white text-center py-3">
+    <footer class="bg-dark text-white text-center py-3 mt-5">
 
         <p class="mb-0">
             Sistema MVC de Cadastros
